9ff6658122facb84e388b4c93c14c2b32a6ca3b9
230049 <230049@edu.p.lodz.pl>
2021-03-15 09:15:25 +0100

    Ive added iterationMethod in falsi regula class (wtf czemu pisze po angielsku)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,13 +4,15 @@ public class App {
 
         Functions functions = new Functions();
         BisectionMethod bisectionMethod = new BisectionMethod();
+        FalsiMethod falsiMethod = new FalsiMethod();
 
         double [] dupa = {3, -3, 0, 0};
 
-        System.out.println(functions.horner(2, dupa));
+//        System.out.println(functions.horner(2, dupa));
 
         bisectionMethod.accurateMethod(0.5, 2, 0.000000000001);
         bisectionMethod.iterationMethod(0.5, 2, 1000);
+        falsiMethod.iterationMethod(3, 4, 1);
 
     }
 }
--- src/main/java/FalsiMethod.java
+++ src/main/java/FalsiMethod.java
@@ -5,10 +5,39 @@ public class FalsiMethod implements FindingMethods {
     public void accurateMethod( double a, double b, double epsilon) {
 
         
+        
     }
 
     @Override
     public void iterationMethod( double a, double b, int iteration) {
 
-    }
+        Functions functions = new Functions();
+        if(functions.sinFunction(a)*functions.sinFunction(b) > 0) {
+            System.out.println("X0 does not exist!");
+            return;
+        }
+
+        double x0 = 0;
+
+        for(int i = 0; i < iteration; i++) {
+
+            x0 = (a * functions.sinFunction(b) - b * functions.sinFunction(a)) /
+                    (functions.sinFunction(b) - functions.sinFunction(a));
+
+
+            if(functions.sinFunction(x0) * functions.sinFunction(a) < 0) {
+
+                b = x0;
+            }
+            else if(functions.sinFunction(x0) * functions.sinFunction(b) < 0)  {
+
+                a = x0;
+            }
+        }
+
+        System.out.println(x0);
+        }
+
+
 }
+
