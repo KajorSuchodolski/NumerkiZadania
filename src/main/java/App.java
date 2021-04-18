29dcc2c7427857df17fe46cf52b86b33c98da5e9
230049 <230049@edu.p.lodz.pl>
2021-04-18 15:54:45 +0200

    Program dziala, dla 100 wezlow sie wyrkes wysypuje, do poprawy, zajme sie tym, pozdrawiam

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,9 @@
-
 import com.panayotis.gnuplot.JavaPlot;
 import com.panayotis.gnuplot.plot.DataSetPlot;
 import com.panayotis.gnuplot.style.PlotStyle;
 import com.panayotis.gnuplot.style.Style;
 
+import javax.xml.crypto.Data;
 import java.io.File;
 import java.util.Scanner;
 
@@ -15,7 +15,9 @@ public class App {
 
             JavaPlot plot = new JavaPlot("C:\\gnuplot\\bin\\gnuplot.exe");
 
-            MathFunction function = new PolynomialFunction();
+            MathFunction function = new AbsoluteFunction();
+
+            Newton newton = new Newton(-10, 10, 50, new AbsoluteFunction());
 
             double [][] y = new double[100][2];
             for(int i = 0; i < y.length; i++) {
@@ -24,23 +26,54 @@ public class App {
                 y[i][1] = function.calculate(tmpX);
             }
 
+            double [][] interpolation = newton.interpolation();
+
+
+            //
             PlotStyle functionStyle = new PlotStyle();
             functionStyle.setStyle(Style.LINES);
             functionStyle.setLineWidth(1);
 
+            PlotStyle interStyle = new PlotStyle();
+            interStyle.setStyle(Style.LINES);
+            interStyle.setLineWidth(1);
+
+            PlotStyle nodesStyle = new PlotStyle();
+            nodesStyle.setStyle(Style.POINTS);
+            nodesStyle.setLineWidth(1);
+
+            //
+
             DataSetPlot functionPlot = new DataSetPlot(y);
+            DataSetPlot interPlot = new DataSetPlot(interpolation);
+            DataSetPlot nodePlot = new DataSetPlot(newton.getNodes());
 
+
+            //
             functionPlot.setPlotStyle(functionStyle);
+            interPlot.setPlotStyle(interStyle);
+            nodePlot.setPlotStyle(nodesStyle);
 
+            //DO POPRAWY TO
             plot.setKey(JavaPlot.Key.OUTSIDE);
-            plot.set("xlabel", "'x");
-            plot.set("ylabel", "'y");
-            plot.set("title", "'Funkcja");
+            plot.set("xlabel", "'x'");
+            plot.set("ylabel", "'f(x)'");
+            plot.set("title", "'Funkcja'");
 
+
+            for(int i = 0; i < newton.getNodes().length; i++) {
+                System.out.println(newton.getNodes()[i][0] + " " + newton.getNodes()[i][1]);
+            }
+
+            //
             plot.addPlot(functionPlot);
+            plot.addPlot(interPlot);
+            plot.addPlot(nodePlot);
 
+            //
             plot.newGraph();
             plot.plot();
+
         }
 
         catch(Exception e) {
--- src/main/java/Newton.java
+++ src/main/java/Newton.java
@@ -1,33 +1,100 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Newton {
 
-    private double [][] nodes;
-    private MathFunction mathFunction;
+    final private double a;
+    final private double b;
+    final private int n;
+    final private double [][] nodes;
+    final private MathFunction mathFunction;
 
     public Newton(double a, double b, int n, MathFunction mathFunction) {
-        this.nodes = chebyshev(a, b, n);
+
+        this.a = a;
+        this.b = b;
+        this.n = n;
         this.mathFunction = mathFunction;
+        this.nodes = chebyshev();
+
+
     }
 
-    public double [][] chebyshev( double a, double b, int n) {
+    public double [][] chebyshev() {
         double [][] nodes = new double[n][2];
 
-        for(int i = 0; i < n; i++) {
-            nodes[i][0] = Math.cos(Math.PI*(2*i + 1) / (2*n + 1));
-            nodes[i][0] = 0.5 * (nodes[i][0] * (b - a) + (a + b));
-            nodes[i][1] = mathFunction.calculate(nodes[i][0]);
+        for(int i = 1; i <= n; i++) {
+
+            //TUTAJ MOZNA INACZEJ
+            nodes[i - 1][0] = Math.cos(Math.PI*(2*i - 1) / (2*n));
+            nodes[i - 1][0] = 0.5 * ((nodes[i - 1][0] * (b - a)) + (a + b));
+            nodes[i - 1][1] = mathFunction.calculate(nodes[i - 1][0]);
         }
 
         return nodes;
     }
 
-    public double [] factors(int a, int b, int n) {
 
-        return null;
+    //te factorsy to tak naprawde wartosci funkcji potrzebne do newtona
+    public double [] factors() {
+
+        double [] factors = new double[n+1];
+
+        for(int i=0; i < n-1; i++) {
+
+            factors[i] = mathFunction.calculate(nodes[i][0]);
+        }
+
+        for(int i = 0; i < n -1; i++) {
+            for(int j = n - 1; j > i; j--) {
+                factors[j] = (factors[j] - factors[j - 1]) / (nodes[j][0] - nodes[j - i -1][0]);
+            }
+        }
+
+        return factors;
     }
 
+    public double [][] interpolation() {
+
+        List<Double> iX = new ArrayList<Double>();
+        List<Double> iY = new ArrayList<Double>();
 
 
-    public double[] getNodes() {
+        double [] factors = factors();
+
+        double mul = 0;
+        double sum = 0;
+
+        for(double x = a; x < b + 0.1; x += 0.1) {
+
+            iX.add(x);
+            for(int i = 0; i <= n; i++) {
+                if(factors[i] != 0) {
+                    mul = factors[i];
+
+                    for(int j = 0; j < i; j++) {
+                        mul = mul * (x - nodes[j][0]);
+                    }
+                    sum += mul;
+                }
+            }
+            iY.add(sum);
+            sum = 0;
+        }
+
+        double [][] interpolation = new double[iX.size()][2];
+
+        for(int i = 0; i < iX.size() ; i++) {
+            interpolation[i][0] = iX.get(i);
+            interpolation[i][1] = iY.get(i);
+
+        }
+
+        return interpolation;
+
+    }
+
+    public double[][] getNodes() {
         return nodes;
     }
 }
