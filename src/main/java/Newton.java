f1e16b9d3449b685f5e1fb120e28a945700969c1
230049 <230049@edu.p.lodz.pl>
2021-04-18 13:39:15 +0200

    Jest

--- src/main/java/Newton.java
+++ src/main/java/Newton.java
@@ -1,21 +1,31 @@
 public class Newton {
 
-    private double [] nodes;
+    private double [][] nodes;
+    private MathFunction mathFunction;
 
-    public Newton(double a, double b, int n) {
+    public Newton(double a, double b, int n, MathFunction mathFunction) {
         this.nodes = chebyshev(a, b, n);
+        this.mathFunction = mathFunction;
     }
 
-    public double [] chebyshev( double a, double b, int n) {
-        double [] nodes = new double[n];
+    public double [][] chebyshev( double a, double b, int n) {
+        double [][] nodes = new double[n][2];
 
         for(int i = 0; i < n; i++) {
-            nodes[i] = Math.cos(Math.PI*(2*i + 1) / (2*n + 1));
-            nodes[i] = 0.5 * (nodes[i] * (b - a) + (a + b));
+            nodes[i][0] = Math.cos(Math.PI*(2*i + 1) / (2*n + 1));
+            nodes[i][0] = 0.5 * (nodes[i][0] * (b - a) + (a + b));
+            nodes[i][1] = mathFunction.calculate(nodes[i][0]);
         }
 
         return nodes;
     }
+
+    public double [] factors(int a, int b, int n) {
+
+        return null;
+    }
+
+
 
     public double[] getNodes() {
         return nodes;
