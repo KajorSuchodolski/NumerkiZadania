694c7337289acf151f82ea65c3a34fd80811e325
mdzieciuchowicz <229874@edu.p.lodz.pl>
2021-05-07 19:24:47 +0200

    init zad4

--- src/main/java/AbsoluteFunction.java
+++
@@ -1,7 +0,0 @@
-public class AbsoluteFunction implements MathFunction {
-
-    @Override
-    public double calculate(double x) {
-        return Math.abs(x);
-    }
-}
--- src/main/java/MathFunction.java
+++
@@ -1,5 +0,0 @@
-public interface MathFunction {
-
-    double calculate(double x);
-}
-
