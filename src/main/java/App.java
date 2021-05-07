9c15435579e5039f7cf3f369f5d041d6f1d41aa1
mdzieciuchowicz <229874@edu.p.lodz.pl>
2021-05-07 22:17:16 +0200

    z jakiegos powodu nie dziala

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,26 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        double eps = 0.0001;
 
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            MathFunction function = null;
+
+            System.out.println("Choose function");
+            function = TUIMethods.chooseFunction();
+
+            Integrals integrals = new Integrals();
+
+            System.out.println("Type number of nodes: ");
+            int n = scanner.nextInt();
+            System.out.println("Do you want to use Gaussian method? [y/n]");
+            double res = (scanner.next().charAt(0) == 'y') ? integrals.GaussChebyshev(function,  n) : integrals.Simpson(function, -1, 1, eps);
+
+            System.out.println("Integral value: " + res);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
 }
